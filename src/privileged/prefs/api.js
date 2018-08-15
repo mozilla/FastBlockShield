7bafbc9dcbacccbc37d533e2b738aebd5c9c5d50
Erica Wright <ericaewright@gmail.com>
2018-08-15 16:52:47 -0400

    Cleanup prefs and notification bar on uninstall.

--- src/privileged/prefs/api.js
+++ src/privileged/prefs/api.js
@@ -12,4 +12,15 @@ this.prefs = class extends ExtensionAPI {
       prefs: Services.prefs
     };
   }
+
+  // Cleanup prefs from here because the cleanup methods in the extension are not properly triggering.
+  // See for progress: https://github.com/mozilla/shield-studies-addon-utils/issues/246
+  onShutdown(shutdownReason) {
+    // Reset these to default values (we should not have recruited anyone who did not originally have default values here)
+    Services.prefs.clearUserPref("privacy.fastblock.list");
+    Services.prefs.clearUserPref("browser.fastblock.enabled");
+    Services.prefs.clearUserPref("browser.fastblock.timeout");
+    Services.prefs.clearUserPref("privacy.trackingprotection.enabled");
+    Services.prefs.clearUserPref("network.http.tailing.enabled")
+  }
 };
